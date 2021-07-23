81fdb34b6cfbe24d0f91d0ea45d7b7fe608ffac8
zhengshuaibing <zhengshuaibing@huohua.cn>
2021-07-23 12:48:42 +0800

    收藏博客地址

---
+++ component/src/main/java/com/circuit/demo/BolgList.java
@@ -0,0 +1,84 @@
+package com.circuit.demo;
+
+public class BolgList {
+
+    /**
+     * 博客地址：
+     *
+     * https://blog.csdn.net/qq_36434742/article/details/108157556
+     * https://blog.csdn.net/qq_36434742/category_10250077.html
+     * https://blog.csdn.net/zc19921215/article/details/83029952
+     * https://blog.csdn.net/qq_40303781/article/details/88667858
+     * https://blog.csdn.net/weixin_43433164/article/details/106925442
+     * https://www.cnblogs.com/weianlai/p/11358768.html
+     * https://blog.csdn.net/mary19920410/article/details/58030147
+     * https://blog.csdn.net/quhongwei_zhanqiu/article/details/39153681
+     * https://blog.csdn.net/quhongwei_zhanqiu/article/details/39153681
+     * https://blog.csdn.net/baidu_27622303/article/details/80558918
+     * https://www.cnblogs.com/lomoye/p/13498043.html
+     * https://www.cnblogs.com/sijidou/p/13121332.html
+     * https://www.cnblogs.com/sijidou/p/13121305.html
+     * https://blog.csdn.net/prestigeding/article/details/78885420
+     * https://www.cnblogs.com/xuxueli/p/5021979.html
+     * https://www.javagreat.com/view/181.html
+     * https://blog.csdn.net/besto229/article/details/106493222/
+     * https://netty.io/wiki/related-projects.html
+     * https://ld246.com/article/1617104943096
+     * https://blog.csdn.net/qq_43571807/article/details/108895606
+     * https://blog.csdn.net/Future_LL/article/details/103163827
+     * https://blog.csdn.net/com_ma/article/details/80459945
+     * https://www.cnblogs.com/panwenbin-logs/p/8007348.html
+     * https://www.cnblogs.com/ichar/p/14168918.html
+     * https://blog.csdn.net/wsb8233696/article/details/107820609
+     * https://www.cnblogs.com/kebibuluan/p/14432019.html
+     * https://blog.csdn.net/a128953ad/article/details/50509437
+     * https://zhuanlan.zhihu.com/p/25459839
+     * https://nacos.io/zh-cn/docs/what-is-nacos.html
+     * https://www.cnblogs.com/java-chen-hao/p/11829344.html
+     * https://mp.weixin.qq.com/s/7bygZNOr_mdCWpF9PrZcFg
+     * https://www.zhihu.com/question/28300645
+     *
+     * https://vip.tulingxueyuan.cn/page/1636680
+     *
+     * https://mp.weixin.qq.com/s/h8ayDuk7SSQ-0-jNmGmydw
+     * https://blog.csdn.net/dingfei136913/article/details/107223123/
+     * https://aistudio.baidu.com/aistudio/index
+     * http://maven.bg.huohua.cn/#browse/search/maven
+     * https://www.cnblogs.com/struggle-1216/p/12210076.html
+     *
+     * https://dubbo.apache.org/zh/docs/v3.0/introduction/
+     *
+     * https://mp.weixin.qq.com/s/dxfJU8E0FAjV5vi_jMT0Gw
+     * https://raw.githubusercontent.com/kubernetes/kubernetes/release-1.2/docs/design/architecture.png
+     *
+     * #java ldap
+     * https://docs.spring.io/spring-ldap/docs/current/reference/#ldap-query-builder-parameters
+     *
+     *
+     * 经典：
+     *
+     * https://blog.csdn.net/bskfnvjtlyzmv867/category_9278567.html
+     * https://blog.csdn.net/javazejian
+     * https://leetcode-cn.com/
+     * https://blog.csdn.net/java_lyvee
+     *
+     * https://github.com/firstgitlub/spring-boot/tree/main/spring-boot-project/spring-boot-starters
+     *
+     * https://www.ics.uci.edu/~fielding/pubs/dissertation/top.htm
+     * http://idea.medeming.com/jets/
+     *
+     * #effective java 翻译作者
+     * https://blog.csdn.net/YuLimin/article/details/102827865?spm=1001.2014.3001.5502
+     *
+     * https://www.kubernetes.org.cn/kubernetes%E8%AE%BE%E8%AE%A1%E6%9E%B6%E6%9E%84
+     *
+     * #领域驱动
+     * https://static-acl-001.geekbang.org/resource/ebook/bf/bd/bf54748a33643e667156110202082cbd.pdf?auth_key=1626923235-0e2f14acf9f14583bd712357845b0ec9-0-b3d40d166ac7372098600179c4f53546
+     *
+     *
+     *
+     *
+     *
+     *
+     */
+}
